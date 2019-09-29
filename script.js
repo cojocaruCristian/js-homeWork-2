70880e98e90779cd3ed86d0ae908707d543ad9a0
CojocaruCristian <51245957+cojocaruCristian@users.noreply.github.com>
2019-09-29 18:59:55 +0300

    first js program

---
+++ script.js
@@ -0,0 +1,33 @@
+var wkDay = prompt("Enter week day");
+
+if (wkDay == "Monday" || wkDay == "Tuesday" || wkDay == "Wednesday" || wkDay == "Thursday" || wkDay == "Friday") {
+    alert("Working day");
+}else if(wkDay == "Suturday" || wkDay == "Sunday"){
+    alert("Weekend");
+}
+
+// switch(wkDay) {
+//     case "Monday":
+//         alert("Working day");
+//         break;
+//     case "Tuesday":
+//         alert("Working day");
+//         break;
+//     case "Wednesday":
+//         alert("Working day");
+//         break;
+//     case "Thursday ":
+//         alert("Working day");
+//         break;
+//     case "Friday":
+//         alert("Working day");
+//         break;
+//     case "Saturday":
+//         alert("Weekend");
+//         break;
+//     case "Sunday":
+//         alert("Weekend");
+//         break;
+//     default:
+//         alert("Can not define, please try another value");
+// }
